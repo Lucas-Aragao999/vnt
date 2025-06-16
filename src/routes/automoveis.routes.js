c9ff22bb998ebd30ded462dc8d610b714ff4bcba
Lucas aragao <luc4s.arara@gmail.com>
2025-06-16 17:31:38 -0300

    v3

--- src/routes/automoveis.routes.js
+++ src/routes/automoveis.routes.js
@@ -10,18 +10,31 @@ router.get("/", (req, res)=>{
     res.json(automoveis);
 });
 
-router.put("/", (req, res)=>{
-    const{id,modelo,marca,ano} = req.body
-    res.json(automoveis); 
+router.put("/id", (req, res)=>{
+    const id = parseInt(req.params.id);
+    const index = automoveis.findIndex(automovel => automovel.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({message: "Automóvel não encontrado"});
+    }
+
+    automoveis[index] = { id,  ...req.body, };
+    res.json(automoveis[index]);
 });
 
-router.post("/", (req,res)=>{
-    const{id,modelo,marca,ano} = req.body
-    res.json(automoveis);  
+router.post("/automoveis", (req,res)=>{
+    const novo = {
+        id: automoveis.length + 1,
+        ...req.body,
+    };
+    automoveis.push(novo);
+    res.status(201).json(novo);
 });
 
-router.delete("/", (req, res)=>{
-    const{id} = req.body;
+router.delete("/automoveis", (req, res)=>{
+    const id = parseInt(req.params.id);
     automoveis = automoveis.filter(automovel => automovel.id !== id);
-    res.json(automoveis);
+    res.json({message: "automovel deletado com sucesso"});
 });
+
+export default router;
